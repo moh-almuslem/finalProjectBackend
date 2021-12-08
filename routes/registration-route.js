2e378d041d1e2fe496d9f95bc12f9fad287bfa29
Mohammad Almuslem <moh.almuslem@gmail.com>
2021-12-08 21:21:31 +0300

    Created backend files, user-routes, registration and login routes

---
+++ routes/registration-route.js
@@ -0,0 +1,72 @@
+function registerUser() {
+    let formData = {
+        "firstName": "",
+        "lastName": "",
+        "email": "",
+        "password": "",
+        "phone": ""
+    }
+
+   // 1.1 use document to get all of the text input elements
+    let textElements = document.getElementsByClassName('field');
+
+    // 1.1.2. create variables for each text field
+    let firstName = textElements[0];
+    let lastName = textElements[1];
+    let email = textElements[2];
+    let password = textElements[3];
+    let phone = textElements[4];
+
+    // 1.2
+    // let checkBox = document.getElementsByClassName('checkbox')[0];
+
+    // 3. validate the form data
+    let errors = [];
+
+    if(firstName.value === '') {
+        errors.push('Please enter first name')
+    }
+
+    if(lastName.value === '') {
+        errors.push('Please enter last name')
+    }
+
+    if(email.value === '') {
+        errors.push('Please enter email')
+    }
+
+    if(password.value === '') {
+        errors.push('Please enter password')
+    }
+    // if (checkBox.checked === false) {
+    //     errors.push('Please accept the terms & conditions')
+    // }
+
+    // Get the hidden div layers
+    let errorBox = document.getElementsByClassName('user-errors')[0];
+    let successBox = document.getElementsByClassName('user-success')[0];
+    if(errors.length > 0) {
+        
+        // Get the hidden div layer with class 'user-errors'
+
+        // hide the success box because an error has occcured
+        successBox.style.display = "none";
+        // Unhide by changing its CSS to 'display: block'
+        errorBox.style.display = "block";
+        // put the errors as html int the 'user-errors' div
+        errorBox.innerHTML = errors.join("<br/>");
+
+    } else {
+        // hide the error box because no error has occcured
+        errorBox.style.display = "none";
+        
+        successBox.style.display = "block";
+    }
+    // 4. send formData to backend
+    formData['firstName'] = firstName.value;
+    formData['lastName'] = lastName.value;
+    formData['email'] = email.value;
+    formData['phone'] = phone.value;
+    formData['password'] = password.value;
+}
+
---
+++ server.js
@@ -0,0 +1,108 @@
+// Importing express as a function
+const express = require('express');
+
+// Import dotenv
+require('dotenv').config();
+
+// CORS (Cross-Origin Resource Sharing)
+const cors = require('cors');
+
+// Import express-form-data to process HTTP POST requests
+const expressFormData = require('express-form-data');
+
+// Import mongoose to connect to MongoDB Atlas
+const mongoose = require('mongoose');
+const UserModel = require('./models/UserModel.js');
+const userRoutes = require('./routes/user-routes.js');
+// Use passport, passport-jwt to read the clien't jwt
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const jwtSecret = process.env.JWT_SECRET;
+
+const passportJwtOptions = {
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: jwtSecret,
+}
+
+// This function will tell passport how what to do
+// with the payload.
+const passportJwt = (passport) => {
+    passport.use(
+        new JwtStrategy(
+            passportJwtOptions,
+            (jwtPayload, done) => {
+
+                // Tell passport what to do with payload
+                UserModel
+                .findOne({ _id: jwtPayload._id })
+                .then(
+                    (dbDocument) => {
+                        // The done() function will pass the 
+                        // dbDocument to Express. The user's 
+                        // document can then be access via req.user
+                        return done(null, dbDocument)
+                    }
+                )
+                .catch(
+                    (err) => {
+                        // If the _id or anything is invalid,
+                        // pass 'null' to Express.
+                        if(err) {
+                            console.log(err);
+                        }
+                        return done(null, null)
+                    }
+                )
+
+            }
+        )
+    )
+};
+passportJwt(passport)
+
+
+// Run the express function to get the methods
+const server = express(); 
+
+// Configure express-form-data for server
+// Is for reading HTML form data
+server.use( expressFormData.parse() );
+
+
+// Declare the connnection string
+const connectionString = process.env.MONGODB_CONNECTION_STRING;
+
+// Create the mongoose config object
+const connectionConfig = {
+    'useNewUrlParser': true,
+    'useUnifiedTopology': true
+}
+
+// Use the .connect() method to connect to MongoDB
+mongoose
+.connect( connectionString, connectionConfig )
+// If connection successful, then...
+.then(
+    function() {
+        console.log('DB is connected');
+    }
+)
+.catch(
+    function() {
+        console.log('DB not connected');
+    }
+)
+
+
+server.use(
+    '/user', userRoutes
+);
+
+// This is the last thing in your file
+server.listen(
+    process.env.PORT || 3001,
+    function() {
+        console.log('connected to http://localhost:3001/');
+    }
+);
